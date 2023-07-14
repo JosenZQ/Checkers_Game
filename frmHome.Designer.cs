ec6a215ef552054a3ec4fceb1de256f13da267c0
JosenZQ <josenzamoraquesada02@gmail.com>
2023-07-13 18:06:01 -0600

    Diseño inicial de Página de Inicio

---
+++ frmHome.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Checkers_Game
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
+            this.lblHomeTitle = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.pbxHomeBackground = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHomeBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHomeTitle
+            // 
+            this.lblHomeTitle.AutoSize = true;
+            this.lblHomeTitle.Font = new System.Drawing.Font("Pixel-Art", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHomeTitle.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblHomeTitle.Location = new System.Drawing.Point(36, 255);
+            this.lblHomeTitle.Name = "lblHomeTitle";
+            this.lblHomeTitle.Size = new System.Drawing.Size(330, 29);
+            this.lblHomeTitle.TabIndex = 1;
+            this.lblHomeTitle.Text = "Checkers Game";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Checkers_Game.Properties.Resources.cancel_button;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(210, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 65);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPlay.BackgroundImage")));
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.Location = new System.Drawing.Point(41, 336);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(150, 65);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.UseVisualStyleBackColor = false;
+            // 
+            // pbxHomeBackground
+            // 
+            this.pbxHomeBackground.Location = new System.Drawing.Point(-1, 0);
+            this.pbxHomeBackground.Name = "pbxHomeBackground";
+            this.pbxHomeBackground.Size = new System.Drawing.Size(416, 576);
+            this.pbxHomeBackground.TabIndex = 0;
+            this.pbxHomeBackground.TabStop = false;
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 575);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblHomeTitle);
+            this.Controls.Add(this.pbxHomeBackground);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Checkers Game";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHomeBackground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxHomeBackground;
+        private System.Windows.Forms.Label lblHomeTitle;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ frmHome.cs
@@ -0,0 +1,33 @@
+﻿using Checkers_Game.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkers_Game
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            pbxHomeBackground.Image = Resources.pixel_art_castle;
+            pbxHomeBackground.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            lblHomeTitle.Parent = pbxHomeBackground;
+            lblHomeTitle.BackColor = Color.Transparent;
+
+            btnPlay.Parent = pbxHomeBackground;
+            btnPlay.BackColor = Color.Transparent; 
+        }
+    }
+}
