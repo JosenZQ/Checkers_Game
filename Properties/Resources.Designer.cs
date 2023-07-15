afed87af24a51bf9ecc9874efbdda43baa38016e
JosenZQ <josenzamoraquesada02@gmail.com>
2023-07-15 10:03:51 -0600

    PictureBox listos en TableLayoutPanel

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace Checkers_Game.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap board_black_token {
+            get {
+                object obj = ResourceManager.GetObject("board_black_token", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap board_white_token {
+            get {
+                object obj = ResourceManager.GetObject("board_white_token", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cancel_button {
             get {
                 object obj = ResourceManager.GetObject("cancel_button", resourceCulture);
